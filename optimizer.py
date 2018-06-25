797dd07777a79b3c8404bfe851f277ea92178251
Andrew Driban <dribaan@habasitusa.net>
2018-06-25 14:44:19 -0400

    exe ready to use

--- optimizer.py
+++ optimizer.py
@@ -24,39 +24,34 @@ def optimizer(file_path, file_name):
     #reading in excel file containing job_slit_status data, converting to array, extracting lot widths column
     df = pandas.read_excel(file, header=None)
     df_arr = df.values
-    mats = df_arr[1:-1,5]
-    lot_widths = df_arr[1:-1,13]
+    mats = df_arr[1:,5]
+    lot_widths = df_arr[1:,13]
     ei_mats_arr = ["A90", "A150", "A120", "APH", "PM", "RPH", "TM", "UM"]
 
     #for loop to count number of jobs at each size
     for index, item in enumerate(lot_widths):
-        
-        #check to see if job is for 300mm slitter
-        if int(item) <= 300:
-            thcount += 1
-            
-        #check if job is meant for 600mm slitter
-        elif int(item) <=600:
-            shcount += 1
-
-        #check if job is meant for 1.2m slitter
-        elif int(item) <=1200:
-            if not any(substring in mats[index] for substring in ei_mats_arr):
+        #check if 80in material
+        if not any(substring in mats[index] for substring in ei_mats_arr):
+            #check which slitter it belongs at
+            if int(item) <= 300:
+                thcount += 1
+            elif int(item) <= 600:
+                shcount += 1
+            elif int(item) <= 1200:
                 twhcount += 1
-
-        #check if job is meant for 2m slitter
-        elif int(item) <=2000:
-            if not any(substring in mats[index] for substring in ei_mats_arr):
+            elif int(item) <= 2000:
                 tmcount += 1
-
-        #check if job is meant for 3m slitter
-        elif int(item) > 2000:
-            if not any(substring in mats[index] for substring in ei_mats_arr):
+            elif int(item) > 2000:
                 thmcount += 1
-            
-        #check if job is meant for 80in slitters
-        else:
-            eicount += 1
+        #if 80in material
+        elif any(substring in mats[index] for substring in ei_mats_arr):
+            #check which slitter it belongs at
+            if int(item) <= 300:
+                thcount += 1
+            elif int(item) <= 600: 
+                shcount += 1
+            elif int(item) > 600:
+                eicount += 1
 
     plot_arr = [thcount, shcount, twhcount, tmcount, thmcount, eicount]
     x = ["300mm", "600mm", "1.2m", "2m", "3m", "80in"]
@@ -64,6 +59,8 @@ def optimizer(file_path, file_name):
     plt.bar(x, plot_arr)
     plt.ylabel("Lot Width (m)")
     plt.title("Slitter Utilization")
+    for a,b in zip(x, plot_arr):
+        plt.text(a,b,str(b))
     plt.show()
 
 optimizer(file_path, file_name)
